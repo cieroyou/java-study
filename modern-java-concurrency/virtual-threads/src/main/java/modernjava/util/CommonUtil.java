8714de07d56f2cddc770f5a3f890ff88a50abaf9
cieroyou <cieroyou1@gmail.com>
2024-05-07 17:20:23 +0900

    [modern-java-concurrency] util 성 코드 추가. LoggerUtil, CommonUtil 추가

---
+++ modern-java-concurrency/virtual-threads/src/main/java/modernjava/util/CommonUtil.java
@@ -0,0 +1,52 @@
+package modernjava.util;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.time.Duration;
+
+import static java.net.http.HttpRequest.newBuilder;
+import static modernjava.util.LoggerUtil.*;
+
+public class CommonUtil {
+
+    public static final HttpClient httpClient = HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(2))
+            .build();
+
+    public  static final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+    public static HttpRequest requestBuilder(String url) {
+        return newBuilder()
+                .uri(URI.create(url))
+                .header("Accept", "application/json")
+                .GET() // default (could leave that out)
+                .build();
+    }
+
+    public static void sleep(long delayMilliSeconds)  {
+        try{
+            Thread.sleep(delayMilliSeconds);
+        }catch (Exception e){
+            log("Exception is :" + e.getMessage());
+        }
+
+    }
+
+
+    public static  int noOfCores(){
+        return Runtime.getRuntime().availableProcessors();
+    }
+
+    public static void main(String[] args) {
+        var cores = noOfCores();
+        log("no of cores : "+ cores);
+    }
+}
---
+++ modern-java-concurrency/virtual-threads/src/main/java/modernjava/util/LoggerUtil.java
@@ -0,0 +1,10 @@
+package modernjava.util;
+
+public class LoggerUtil {
+
+    public static void log(String message){
+
+        System.out.println("[" + Thread.currentThread() +"] - " + message);
+
+    }
+}
